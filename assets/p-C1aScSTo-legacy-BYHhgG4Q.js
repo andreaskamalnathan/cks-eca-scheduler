efea4e14b19be0f69c13f46bfeb011d72f1d521d
Andreas <andreaskamalnathan@gmail.com>
2026-06-29 12:03:18 +0700

    Updates

--- assets/p-C1aScSTo-legacy-BYHhgG4Q.js
+++ assets/p-C1aScSTo-legacy-BYHhgG4Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fSUuEUGi.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
+System.register(["./index-legacy-DsNALEZD.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
